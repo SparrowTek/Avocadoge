7b16204ff6b315f420cdb9a6b4d9e084fce11b79
Thomas Rademaker <Thomas@SparrowTek.com>
2021-02-14 14:12:40 -0500

    create Xcode project

---
+++ Avocadoge/AvocadogeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadogeApp.swift
+//  Avocadoge
+//
+//  Created by Thomas Rademaker on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadogeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
